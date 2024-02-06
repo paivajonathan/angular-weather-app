b4a3bd514282604a8428aedf0bdca71bc37ed57c
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-02-06 19:06:31 -0300

    feat(project): add files

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherData } from 'src/app/models/interfaces/weather.interface';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent implements OnInit {
+  @Input() weatherDataInput!: WeatherData;
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+
+  ngOnInit(): void {
+    console.log('Dados recebidos do componente pai:', this.weatherDataInput);
+  }
+}
---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey: string = 'e4d8ebe89a4f7209a1d45ea80ed370ae';
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
+}
